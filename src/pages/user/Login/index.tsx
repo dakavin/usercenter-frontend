2558a0c3211e3d92d4ef62f2f6ec8e543f26ed49
dakavin <mikeylay@126.com>
2024-04-15 08:11:40 +0800

    修复登录页面报错问题

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -27,7 +27,7 @@ const LoginMessage: React.FC<{
   />
 );
 const Login: React.FC = () => {
-  const [userLoginState, setUserLoginState] = useState<API.LoginResult>({});
+  const [userLoginState] = useState<API.LoginResult>({});
   const [type, setType] = useState<string>('account');
   const { initialState, setInitialState } = useModel('@@initialState');
   const fetchUserInfo = async () => {
@@ -62,8 +62,6 @@ const Login: React.FC = () => {
         history.push(redirect || '/');
         return;
       }
-      // 如果失败去设置用户错误信息
-      setUserLoginState(user);
     } catch (error) {
       const defaultLoginFailureMessage = '登录失败，请重试！';
       message.error(defaultLoginFailureMessage);
